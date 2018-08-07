dcfbc6887178f4f430a410ea9a04273572f751f2
Robin Sola <rsolarms@gmail.com>
2018-08-07 15:01:13 -0700

    create first php index

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+  $message = "Hello World!";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Lessons</title>
+  </head>
+  <body>
+    <h1><?php echo $message; ?></h1>
+  </body>
+</html>
